70dd0557fc226cfcd41c617aec5e9ee4fce4afe2
Sean Zheng <seanzhengw@gmail.com>
2020-01-11 23:50:07 +0800

    Add group command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,33 +7,39 @@ set -e
     exit 1;
 };
 
-echo "pwd"
-pwd
-
 docs_src=$GITHUB_WORKSPACE/docs
 docs_html=$GITHUB_WORKSPACE/gh-pages
 sphinx_doctree=$GITHUB_WORKSPACE/.doctree
 
+echo ::group::Create working directories
 echo "mkdir $docs_src"
 mkdir $docs_src
 echo "mkdir $docs_html"
 mkdir $docs_html
 echo "mkdir $sphinx_doctree"
 mkdir $sphinx_doctree
+echo ::endgroup::
 
 # checkout branch docs
+echo ::group::Initializing the repository
 echo "cd $docs_src"
 cd $docs_src
 echo "git init"
 git init
 echo "git remote add origin https://github.com/$GITHUB_REPOSITORY.git"
 git remote add origin https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+echo ::endgroup::
+echo ::group::Fetching the repository
 echo "git fetch origin +$GITHUB_SHA:refs/remotes/origin/docs"
 git fetch origin +$GITHUB_SHA:refs/remotes/origin/docs
+echo ::endgroup::
+echo ::group::Checkout ref 
 echo "git checkout -B docs refs/remotes/origin/docs"
 git checkout -B docs refs/remotes/origin/docs
-echo "git log -1"
+echo ::endgroup::
+echo ::group::Show HEAD message
 git log -1
+echo ::endgroup::
 
 # get author
 author_name="$(git show --format=%an -s)"
@@ -47,53 +53,75 @@ echo "::set-output name=docs_sha::$(echo ${GITHUB_SHA})"
 echo "::set-output name=docs_sha8::"$docs_sha8""
 
 # checkout branch gh-pages
+echo ::group::Initializing branch gh-pages
 echo "cd $docs_html"
 cd $docs_html
 echo "git init"
 git init
 echo "git remote add origin https://github.com/$GITHUB_REPOSITORY.git"
 git remote add origin https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
+echo ::endgroup::
 
 # check remote branch exist first
-echo "Checking remote branch gh-pages."
+echo ::group::Check remote branch gh-pages exist
+echo "git ls-remote --heads origin refs/heads/gh-pages"
 gh_pages_exist=$(git ls-remote --heads origin refs/heads/gh-pages)
 if [ -z "$gh_pages_exist" ] 
+then
+    echo "Not exist."
+else
+    echo "Exist"
+fi
+echo ::endgroup::
+
+if [ -z "$gh_pages_exist" ] 
 then 
-    echo "-> remote branch gh-pages not exist, auto create."
+    echo ::group::Create branch gh-pages
     echo "git checkout -B gh-pages"
     git checkout -B gh-pages
+    echo ::endgroup::
 else
-    echo "-> remote branch gh-pages exist, fetch."
+    echo ::group::Fetching branch gh-pages
     echo "git fetch origin +refs/heads/gh-pages:refs/remotes/origin/gh-pages"
     git fetch origin +refs/heads/gh-pages:refs/remotes/origin/gh-pages
     echo "git checkout -B gh-pages refs/remotes/origin/gh-pages"
     git checkout -B gh-pages refs/remotes/origin/gh-pages
     echo "git log -1"
     git log -1
+    echo ::endgroup::
 fi
 
 # git config
+echo ::group::Set commiter
 echo "git config user.name $author_name"
 git config user.name $author_name
 echo "git config user.email $author_email"
 git config user.email $author_email
+echo ::endgroup::
 
 # sphinx-build
+echo ::group::Sphinx build html
 echo "sphinx-build -b html $docs_src/source $docs_html -E -d $sphinx_doctree"
 sphinx-build -b html $docs_src/source $docs_html -E -d $sphinx_doctree
+echo ::endgroup::
 
 # auto creation of README.md
 if [ "$INPUT_CREATE_README" = true ] ; then
-    echo "Create file README.md"
+    echo ::group::Create README
+    echo "Create file README.md with these content"
     echo "GitHub Pages of [$GITHUB_REPOSITORY](https://github.com/$GITHUB_REPOSITORY.git)" > README.md
     echo "===" >> README.md
     echo "Sphinx html documentation of [$docs_sha8](https://github.com/$GITHUB_REPOSITORY/tree/$GITHUB_SHA)" >> README.md
+    cat README.md
+    echo ::endgroup::
 fi
 
 # commit and push
+echo ::group::Push
 echo "git add ."
 git add .
-echo 'git commit --allow-empty -m "From $GITHUB_REF"'
+echo 'git commit --allow-empty -m "From $GITHUB_REF $docs_sha8"'
 git commit --allow-empty -m "From $GITHUB_REF $docs_sha8"
 echo "git push origin gh-pages"
 git push origin gh-pages
+echo ::endgroup::
